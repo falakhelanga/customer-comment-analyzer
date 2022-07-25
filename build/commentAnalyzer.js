244073148a29b13763f53653644802fd72a0312e
Falakhe <falakhe@bigbrave.digital>
2022-07-25 19:01:41 +0200

    Clean up ignored files

--- build/commentAnalyzer.js
+++ build/commentAnalyzer.js
@@ -1,46 +1,42 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.commentAnalyzer = void 0;
-const fs_1 = require("fs");
-const readline_1 = __importDefault(require("readline"));
-const path_1 = __importDefault(require("path"));
-let res = {
+exports.CommentAnalyzer = void 0;
+/////////// totals with initial values
+const totals = {
     SHORTER_THAN_15: 0,
     MOVER_MENTIONS: 0,
     SHAKER_MENTION: 0,
     QUESTIONS: 0,
     SPAM: 0,
 };
-const matcher = (condition, type, newValue) => {
-    if (condition) {
-        return res[type] + newValue;
+class CommentAnalyzer {
+    constructor(file, line) {
+        this.totals = totals;
+        /////////// this method helps adding the new metrics.
+        this.finder = (condition, type, newValue) => {
+            if (condition) {
+                return totals[type] + newValue;
+            }
+            return totals[type];
+        };
+        ////////// analyzing the comment
+        this.analyze = () => {
+            this.line.on("line", (line) => {
+                //////////// conditions
+                const moverMatches = line.match(/mover/gi);
+                const shakerMatches = line.match(/shaker/gi);
+                const urlMatch = line.match(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi);
+                const questionsMatches = line.includes("?");
+                const shortMatches = line.trim() !== "" && line.length <= 15;
+                totals.SHAKER_MENTION = this.finder(!!shakerMatches, "SHAKER_MENTION", shakerMatches === null || shakerMatches === void 0 ? void 0 : shakerMatches.length);
+                totals.MOVER_MENTIONS = this.finder(!!moverMatches, "MOVER_MENTIONS", moverMatches === null || moverMatches === void 0 ? void 0 : moverMatches.length);
+                totals.SPAM = this.finder(!!urlMatch, "SPAM", 1);
+                totals.SHORTER_THAN_15 = this.finder(shortMatches, "SHORTER_THAN_15", 1);
+                totals.QUESTIONS = this.finder(questionsMatches, "QUESTIONS", 1);
+            });
+        };
+        this.file = file;
+        this.line = line;
     }
-    return res[type];
-};
-const commentAnalyzer = (file, files, idx) => {
-    const line = readline_1.default.createInterface({
-        input: (0, fs_1.createReadStream)(path_1.default.resolve(`./docs/${file}`)),
-    });
-    line.on("line", (line) => {
-        const moverMatches = line.match(/mover/gi);
-        const shakerMatches = line.match(/shaker/gi);
-        const urlMatch = line.match(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi);
-        const questionMatcher = line.includes("?");
-        const shortMatches = line.length <= 15;
-        res.SHAKER_MENTION = matcher(!!shakerMatches, "SHAKER_MENTION", shakerMatches === null || shakerMatches === void 0 ? void 0 : shakerMatches.length);
-        res.MOVER_MENTIONS = matcher(!!moverMatches, "MOVER_MENTIONS", moverMatches === null || moverMatches === void 0 ? void 0 : moverMatches.length);
-        res.SPAM = matcher(!!urlMatch, "SPAM", 1);
-        res.SHORTER_THAN_15 = matcher(shortMatches, "SHORTER_THAN_15", 1);
-        res.QUESTIONS = matcher(questionMatcher, "QUESTIONS", 1);
-    });
-    line.on("close", () => {
-        if (idx === files.length - 1) {
-            console.log("RESULTS\n=======");
-            console.log(Object.keys(res).forEach((key) => console.log(`${key}: ${res[key]}`)));
-        }
-    });
-};
-exports.commentAnalyzer = commentAnalyzer;
+}
+exports.CommentAnalyzer = CommentAnalyzer;
--- build/index.js
+++ build/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,23 +34,51 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
+const fs_1 = __importStar(require("fs"));
+const readline_1 = __importDefault(require("readline"));
 const path_1 = __importDefault(require("path"));
 const commentAnalyzer_1 = require("./commentAnalyzer");
-const dirPath = path_1.default.resolve("./docs");
-(() => {
-    const files = fs_1.default.readdir(dirPath, (err, files) => {
+class Main {
+}
+_a = Main;
+Main.addToatals = () => {
+    const dirPath = path_1.default.resolve("./docs");
+    fs_1.default.readdir(dirPath, (err, files) => {
         try {
             if (err) {
-                throw new Error("Oops something went wrong!");
+                throw Error("Oops something went wrong!");
             }
+            /////////// looping in all the files
             files.forEach((file, idx) => __awaiter(void 0, void 0, void 0, function* () {
-                (0, commentAnalyzer_1.commentAnalyzer)(file, files, idx);
+                const line = readline_1.default.createInterface({
+                    input: (0, fs_1.createReadStream)(path_1.default.resolve(`./docs/${file}`)),
+                });
+                try {
+                    //////////// check if it is a text file
+                    if (path_1.default.extname(file) !== ".txt") {
+                        throw Error("Only text files allowed");
+                    }
+                    const commentAnalyzer = new commentAnalyzer_1.CommentAnalyzer(file, line);
+                    commentAnalyzer.analyze();
+                    //////////// check if it is the last file and print the results in the console
+                    line.on("close", () => {
+                        const totals = commentAnalyzer.totals;
+                        if (idx === files.length - 1) {
+                            console.log("RESULTS\n=======");
+                            Object.keys(totals).forEach((key) => console.log(`${key}: ${totals[key]}`));
+                        }
+                    });
+                }
+                catch (error) {
+                    console.log(error);
+                }
             }));
         }
         catch (error) {
             console.log(error);
         }
     });
-})();
+};
+Main.addToatals();
